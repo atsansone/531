4fbc96dc40ca3c9bbecb6b684994673db242f3be
Karl Stolley <karl.stolley@gmail.com>
2012-10-01 15:04:53 -0500

    Set up belongs_to/has_many relationships

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ActiveRecord::Base
   attr_accessible :content, :username
+
+  belongs_to :quote
 end
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,3 +1,5 @@
 class Quote < ActiveRecord::Base
   attr_accessible :content, :source
+
+  has_many :comments
 end
