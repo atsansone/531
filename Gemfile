df0d48f81edaf263bffc69cc1c1e1ed685c84789
Karl Stolley <karl.stolley@gmail.com>
2012-10-01 23:43:42 -0500

    Added RDiscount gem to process Markdown, and created two helper methods to do just that

--- Gemfile
+++ Gemfile
@@ -7,6 +7,9 @@ gem 'rails', '3.2.8'
 
 gem 'sqlite3'
 
+# Use rdiscount to process markdown
+gem 'rdiscount'
+
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  #Markdown that preserves tags
+  def md(string)
+    RDiscount.new(string, :smart).to_html.html_safe
+  end
+
+  #Markdown that strips tags (used for one-liners, like titles, to achieve smart quotes)
+  def mds(string)
+    strip_tags(RDiscount.new(string, :smart).to_html)
+  end
+
+
 end
