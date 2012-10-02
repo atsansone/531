dd38ca380e542720dff5657335e5d4a36585957a
Karl Stolley <karl.stolley@gmail.com>
2012-10-01 21:17:02 -0500

    Modified index, create, and delete actions on Comments controller to behave as nested resources

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,21 @@
 class CommentsController < ApplicationController
-  # GET /comments
-  # GET /comments.json
+  # GET quotes/:quote_id/comments
+  # GET quotes/:quote_id/comments.json
   def index
-    @comments = Comment.all
+    # Comment.all is no longer needed, so it's commented out (but left here for reference)
+    # @comments = Comment.all
+
+    # Instead, we get the current Quote based on the URL /quotes/:quote_id/commments
+    # The model association has_many :comments will allow us to grab all comments index
+    # the comments index.html.erb view through the @quote.comments method:
+
+    @quote = Quote.find(params[:quote_id]) # Grab the Quote based on the URL
+
+    # Finally, let's set up a new instance of @comment so that a form appears
+    # at the bottom of the comment listing for someone to write a new Comment
+    # on the current quote
+
+    @comment = Comment.new
 
     respond_to do |format|
       format.html # index.html.erb
@@ -37,18 +50,22 @@ class CommentsController < ApplicationController
     @comment = Comment.find(params[:id])
   end
 
-  # POST /comments
-  # POST /comments.json
+  # POST quotes/:quote_id/comments
+  # POST quotes/:quote_id/comments.json
   def create
-    @comment = Comment.new(params[:comment])
+
+    @quote = Quote.find(params[:quote_id]) # Grab the Quote based on the URL
+
+    #@comment = Comment.new(params[:comment])
+    @comment = @quote.comments.build(params[:comment])
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render json: @comment, status: :created, location: @comment }
+        format.html { redirect_to quote_comments_path(@quote), notice: 'Comment was successfully created.' }
+        #format.json { render json: @comment, status: :created, location: @comment }
       else
-        format.html { render action: "new" }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
+        #format.html { render action: "new" }
+        #format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -69,14 +86,17 @@ class CommentsController < ApplicationController
     end
   end
 
-  # DELETE /comments/1
-  # DELETE /comments/1.json
+  # DELETE quotes/:quote_id/comments/1
+  # DELETE quotes/:quote_id/comments/1.json
   def destroy
+
+    @quote = Quote.find(params[:quote_id]) # Grab the Quote based on the URL
+
     @comment = Comment.find(params[:id])
     @comment.destroy
 
     respond_to do |format|
-      format.html { redirect_to comments_url }
+      format.html { redirect_to quote_comments_path(@quote) }
       format.json { head :no_content }
     end
   end
