42be0a23d25308387f7a5a04491397f02e858065
Karl Stolley <karl.stolley@gmail.com>
2012-10-01 21:19:25 -0500

    Added seed data for Quotes and some sample, associated Comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,3 +26,22 @@ pages = Page.create([
   { title: 'Welcome', content: %{ This is the home page. } },
   { title: 'About', content: %{ This is the about page. } }
   ])
+
+# Now we'll build some more intricate seed data; this sample app allows for the entry
+# of quotes (actually, quotations, but whatevs) and for people to comment on the quotes.
+# To show the associations at work, we'll build a few comments on each quote. Note that
+# the comments are created off of the quote. instance; that is how Rails keeps track of
+# the ID, even for new records, for the quote_id column on Comments.
+
+quote = Quote.create({content: %{ To be, or not to be. }, source: %{ Shakespeare, *Hamlet* }})
+comments = quote.comments.create([
+  { content: %{LOL WOW awesome cool quote.}, username: 'LOLguy' },
+  { content: %{I read this in high school.}, username: 'CaptainObvious' },
+  { content: %{That is the question...or something.}, username: 'AttemptedSnark' }
+])
+
+quote = Quote.create({content: %{ Ask not what your country can do for you -- ask what you can do for your country. }, source: %{ President John F. Kennedy }})
+comments = quote.comments.create([
+  { content: %{Why don't politicians speak like this anymore?}, username: 'NostalgiaFTW' },
+  { content: %{Great quote.}, username: 'CaptainObvious' }
+])
